4f7b674d29dc6e4a7cdeb2ebf315e248b9d19541
parasbramhankar <parasbramhankar.in@gmail.com>
2025-12-01 13:36:40 +0530

    Initial commit

---
+++ src/test/java/com/example/SpringDataJPA_P01/SpringDataJpaP01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringDataJPA_P01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaP01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

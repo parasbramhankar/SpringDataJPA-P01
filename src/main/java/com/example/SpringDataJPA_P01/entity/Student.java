7753d82029cfb7f6dde30c65ce58191c552c1af1
parasbramhankar <parasbramhankar.in@gmail.com>
2025-12-02 13:34:54 +0530

    commit

--- src/main/java/com/example/SpringDataJPA_P01/entity/Student.java
+++ src/main/java/com/example/SpringDataJPA_P01/entity/Student.java
@@ -1,4 +1,60 @@
 package com.example.SpringDataJPA_P01.entity;
 
+
+import jakarta.persistence.*;
+
+@Entity
+//@Table(name="students")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String email;
+    private int age;
+
+
+    Student(){
+
+    }
+
+    public Student(int age, String email, String name) {
+        this.age = age;
+        this.email = email;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }

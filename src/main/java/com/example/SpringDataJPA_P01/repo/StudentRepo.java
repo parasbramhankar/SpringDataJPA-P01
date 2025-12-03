8906642e240399aa9a3c51861eac3be8cbe34d08
parasbramhankar <parasbramhankar.in@gmail.com>
2025-12-03 05:48:37 +0530

    commit

--- src/main/java/com/example/SpringDataJPA_P01/repo/StudentRepo.java
+++ src/main/java/com/example/SpringDataJPA_P01/repo/StudentRepo.java
@@ -6,4 +6,17 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepo extends JpaRepository<Student,Integer> {
+    /*
+    JpaRepository this is a marker interface, JVM is responsible to give its functionality
+    Responsibility is that to give the implementation of the StudentRepo interface internally,
+
+    Implementation: All the methods that are communicate with the database,that all come inside the
+    StudentRepo interface internally, and all this happened automatically.
+
+    Example: save() : use to save the data into the db,  ( use into the Service class)
+             public Student saveStudent(Student student){
+                 return studentRepo.save(student);
+             }
+
+     */
 }

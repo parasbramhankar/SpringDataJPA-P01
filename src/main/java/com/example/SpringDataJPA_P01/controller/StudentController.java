a44e6630c9f6e24b88d25e6acc7f27c1a64b8ff0
parasbramhankar <parasbramhankar.in@gmail.com>
2025-12-03 05:56:42 +0530

    commit

--- src/main/java/com/example/SpringDataJPA_P01/controller/StudentController.java
+++ src/main/java/com/example/SpringDataJPA_P01/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.example.SpringDataJPA_P01.controller;
 import com.example.SpringDataJPA_P01.entity.Student;
 import com.example.SpringDataJPA_P01.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/std")
@@ -20,7 +19,10 @@ public class StudentController {
         return studentService.saveStudent(student);
     }
 
-
+    @GetMapping("/all")
+    public List<Student>getAllStudent(){
+        return studentService.getAllStudentData();
+    }
 
 
 
